fb6956fbca3d61e1fc2a4a95fa2871c140673b50
perry <power80203@gmail.com>
2019-03-29 23:04:12 +0800

    init

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+    
+    def index
+        
+    end
+
+    def first
+
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root "home#index"
+
+    get "first", to: "home#first" # ruby unique syntax
+    get "second", to: "home#second" # ruby unique syntax
+
+end
